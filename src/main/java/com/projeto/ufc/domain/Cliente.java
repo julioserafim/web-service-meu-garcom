cfbc76c3570f53b54ab44d4e8a648118825f3e35
julioserafim <serafimjuliom583@gmail.com>
2016-12-07 00:27:18 -0300

    Última atualização

--- src/main/java/com/projeto/ufc/domain/Cliente.java
+++ src/main/java/com/projeto/ufc/domain/Cliente.java
@@ -1,9 +1,22 @@
 package com.projeto.ufc.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Entity
+@Table(name = "cliente")
 public class Cliente {
+	
+	@JsonInclude(Include.NON_NULL)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private int mesa;
 
